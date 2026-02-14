a292dd7a66ad0a6819615c8d232eeb755fd2375e
ashishmehra69-netizen <ashishmehra69@gmail.com>
2026-02-14 16:20:36 +0530

    Update NeuralBackground.jsx

--- src/components/NeuralBackground.jsx
+++ src/components/NeuralBackground.jsx
@@ -2,7 +2,7 @@ export default function NeuralBackground() {
   return (
     <div 
       className="fixed inset-0 bg-red-500" 
-      style={{ zIndex: 0 }}  // Changed from -1 to 0
+      style={{ zIndex: -1 }}  // Changed from -1 to 0
     >
       <h1 className="text-white text-6xl p-20">NEURAL BACKGROUND TEST</h1>
     </div>
