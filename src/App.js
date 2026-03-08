b549150cb5b45d0123bd6ff8575fe9465b6348e8
ashishmehra69-netizen <ashishmehra69@gmail.com>
2026-03-08 12:20:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import NeuralBackground from './components/NeuralBackground';
 import InputForm from './components/InputForm';
 import ReactMarkdown from 'react-markdown';
